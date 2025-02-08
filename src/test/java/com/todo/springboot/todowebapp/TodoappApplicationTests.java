81e32a100d953f3f7c3ccbefa162cf681b63c453
Alex Rugina <alexrugina8@yahoo.com>
2025-02-08 19:16:47 +0200

    Project Initializer

---
+++ src/test/java/com/todo/springboot/todowebapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.springboot.todowebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
